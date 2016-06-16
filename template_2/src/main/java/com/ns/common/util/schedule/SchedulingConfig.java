8553bb68dbe9ba7ffc130a471e0bf83322aef855
FireElement <pire_cao@163.com>
2016-06-16 15:44:15 +0800

    Add schedule config

---
+++ template_2/src/main/java/com/ns/common/util/schedule/SchedulingConfig.java
@@ -0,0 +1,18 @@
+package com.ns.common.util.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by xuezhucao on 16/6/16.
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+
+    @Scheduled(cron = "0/20 * * * * ?")
+    public void scheduler() {
+        System.out.println(">>>>>>>>> SchedulingConfig.scheduler()");
+    }
+}
