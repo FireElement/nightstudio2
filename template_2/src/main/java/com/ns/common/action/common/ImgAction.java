b47f1c0c165b6e10d5c3a0c2ae2bfea6d17207f6
FireElement <pire_cao@163.com>
2016-11-10 15:40:29 +0800

    Add image upload action

--- template_2/src/main/java/com/ns/common/action/common/ImgAction.java
+++ template_2/src/main/java/com/ns/common/action/common/ImgAction.java
@@ -3,7 +3,6 @@ package com.ns.common.action.common;
 import com.ns.common.util.constant.PathConstant;
 import com.ns.common.util.image.ImgUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImgAction {
 
     @RequestMapping("/upload")
-    public Object upload(@RequestParam String name, String img) throws Throwable {
+    public Object upload(String name, String img) throws Throwable {
         ImgUtil.uploadImage(name, img);
         return null;
     }
