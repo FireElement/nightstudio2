515fa6cf82d5bd56dead1eaa41627c6f1bdea5de
FireElement <pire_cao@163.com>
2016-12-27 14:19:41 +0800

    Modify spring boot time zone

---
+++ template_2/src/main/java/com/ns/common/util/config/MapperScanConfig.java
@@ -0,0 +1,21 @@
+package com.ns.common.util.config;
+
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by xuezhucao on 16/12/26.
+ */
+@Configuration
+public class MapperScanConfig {
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer() {
+        MapperScannerConfigurer mapperScannerConfigurer = new MapperScannerConfigurer();
+        mapperScannerConfigurer.setSqlSessionFactoryBeanName("sqlSessionFactory");
+        mapperScannerConfigurer.setBasePackage("com.ns");
+        return mapperScannerConfigurer;
+    }
+
+}
