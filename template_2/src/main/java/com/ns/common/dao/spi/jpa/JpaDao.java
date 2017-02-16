efa3d1e49660a63b7de986e37ad4e79d24027516
FireElement <pire_cao@163.com>
2017-02-16 18:11:13 +0800

    Modify jpa dao

--- template_2/src/main/java/com/ns/common/dao/spi/jpa/JpaDao.java
+++ template_2/src/main/java/com/ns/common/dao/spi/jpa/JpaDao.java
@@ -1,11 +1,16 @@
 package com.ns.common.dao.spi.jpa;
 
+import org.apache.commons.collections.IteratorUtils;
 import org.springframework.data.repository.CrudRepository;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by caoxuezhu on 2017/2/13.
  */
 public interface JpaDao<T, ID extends Serializable> extends CrudRepository<T, ID> {
+    default List<T> saves(List<T> objs) {
+        return IteratorUtils.toList(save(objs).iterator());
+    }
 }
